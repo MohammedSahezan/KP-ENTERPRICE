e39ca55cbe8905c9400f557836ba41c6a6c40593
MohammedSahezan <patelsahezan28@gmail.com>
2025-04-24 22:37:57 +0530

    add change button

--- FRONTEND/src/Pages/AdminDashboard.jsx
+++ FRONTEND/src/Pages/AdminDashboard.jsx
@@ -213,8 +213,10 @@ function AdminDashboard() {
 
           {isFormOpen && (
             <form
-              className={`space-y-4 p-6 rounded-lg shadow-lg bg-${theme} bg-base-300 text-base-content`}
-            >
+  onSubmit={handleSubmit}
+  className={`space-y-4 p-6 rounded-lg shadow-lg bg-${theme} bg-base-300 text-base-content`}
+>
+
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
                   <label
@@ -307,14 +309,14 @@ function AdminDashboard() {
 
               {/* Submit and Cancel Buttons */}
               <div className="flex gap-4">
-                <button
-                  type="submit"
-                  onClick={handleSubmit}
-                  className="px-4 py-2 rounded hover:opacity-90 transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
-                bg-blue-600 text-white"
-                >
-                  Submit
-                </button>
+              <button
+  type="submit"
+  className="px-4 py-2 rounded hover:opacity-90 transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
+  bg-blue-600 text-white"
+>
+  Submit
+</button>
+
 
                 <button
                   type="button"
